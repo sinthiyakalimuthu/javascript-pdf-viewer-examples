68cf3f85ef744a1073184754742d985c7a1fe245
RanjithaAmirthalingam <63278452+RanjithaAmirthalingam@users.noreply.github.com>
2022-10-10 10:58:15 +0530

    BLAZ-25693 Added the svelte sample.

---
+++ How to/PDF viewer with svelte framework/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	const initializeRemarkable = () => {
+		let pdfViewer = new ej.pdfviewer.PdfViewer({
+			// Change serviceUrl to the host link for your local PDF viewer server.
+			serviceUrl : 'https://ej2services.syncfusion.com/production/web-services/api/pdfviewer',
+			// Change the documentPath to your preferred load document path.
+			documentPath : 'FormDesigner.pdf',
+		});
+	pdfViewer.appendTo('#pdfViewer');
+	}
+</script>
+
+<svelte:head>
+	<!-- Essential JS 2 PDF Viewer's global script -->
+	<script src="https://cdn.syncfusion.com/ej2/20.2.48/dist/ej2.min.js" on:load={initializeRemarkable}></script>
+	<!-- Essential JS 2 PDF Viewer's global style -->
+	<link href="https://cdn.syncfusion.com/ej2/20.2.48/material.css" rel="stylesheet">
+</svelte:head>
+<main>
+	<body style="margin:0px">
+		<div style="height: 100%;width: 100%">
+		    // Add the PDF Viewer.
+			<div id="pdfViewer"style="height: 100vh;width: 100%"></div>
+		</div>
+	</body>
+</main>
+
